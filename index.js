d4e8901d3701a06b4641eca5a33647512023509c
donaldmema <mema.donald@gmail.com>
2022-06-14 01:08:14 +0300

    Tax gets calculated on all different cases

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function netSalary(employeeFullName, grossSalary, pensionContribution) {
   let taxableAmount = grossSalary - grossSalary * pensionContribution;
+  console.log("Taxable Amount: " + taxableAmount);
   let takeHomeSalary;
 
   let taxRateUnder250 = 0.04;
@@ -8,18 +9,24 @@ function netSalary(employeeFullName, grossSalary, pensionContribution) {
 
   if (taxableAmount > 450) {
     const taxUnder250EUR = (250 - 80) * taxRateUnder250;
+    console.log("Tax Under 250: " + taxUnder250EUR);
     const taxUnder450EUR = (450 - 250) * taxRateUnder450;
+    console.log("Tax Under 450: " + taxUnder450EUR);
     const taxOver450EUR = (taxableAmount - 450) * taxRateOver450;
+    console.log("Tax Over 450: " + taxOver450EUR);
     takeHomeSalary =
       taxableAmount - taxUnder250EUR - taxUnder450EUR - taxOver450EUR;
-  } else if (250 <= taxableAmount <= 450) {
+  } else if (250 < taxableAmount && taxableAmount <= 450) {
     const taxUnder250EUR = (250 - 80) * taxRateUnder250;
+    console.log("Tax Under 250: " + taxUnder250EUR);
     const taxUnder450EUR = (taxableAmount - 250) * taxRateUnder450;
+    console.log("Tax Under 450: " + taxUnder450EUR);
     const taxOver450EUR = 0;
     takeHomeSalary =
       taxableAmount - taxUnder250EUR - taxUnder450EUR - taxOver450EUR;
-  } else if (80 < taxableAmount < 250) {
+  } else if (80 < taxableAmount && taxableAmount <= 250) {
     const taxUnder250EUR = (taxableAmount - 80) * taxRateUnder250;
+    console.log("Tax Under 250: " + taxUnder250EUR);
     const taxUnder450EUR = 0;
     const taxOver450EUR = 0;
     takeHomeSalary =
@@ -40,4 +47,8 @@ function netSalary(employeeFullName, grossSalary, pensionContribution) {
 // let grossSalary = 500;
 // let pensionContribution = 0.05;
 
-netSalary("Xhon Cena", 250, 0.05);
+netSalary("Xhon Cena", 500, 0.05);
+netSalary("Xhon Sina", 400, 0.05);
+netSalary("John Cena", 250, 0.05);
+netSalary("John Franklin", 180, 0.05);
+netSalary("Don Juan", 70, 0.05);
